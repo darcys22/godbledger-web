8eff2b62a9493b5c0c5ed7a9bd1d136e351dd614
Sean Darcy <darcys22@gmail.com>
2020-10-09 05:00:53 +0000

    searching for account rather than stub

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/darcys22/godbledger-web
 go 1.13
 
 require (
+	github.com/darcys22/godbledger v0.4.0
 	github.com/gin-gonic/contrib v0.0.0-20201005132743-ca038bbf2944
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-macaron/binding v1.1.1
@@ -12,10 +13,10 @@ require (
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
 	github.com/sirupsen/logrus v1.6.0
 	github.com/unknwon/com v1.0.1 // indirect
+	github.com/urfave/cli/v2 v2.2.0
 	github.com/x-cray/logrus-prefixed-formatter v0.5.2
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
-	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
 	gopkg.in/ini.v1 v1.61.0
 	gopkg.in/macaron.v1 v1.3.9
 )
--- pkg/api/accounts.go
+++ pkg/api/accounts.go
@@ -1,10 +1,17 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
+	"flag"
+
+	"github.com/darcys22/godbledger/godbledger/cmd"
+	"github.com/darcys22/godbledger/godbledger/ledger"
+
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 )
+
+var log = logrus.WithField("prefix", "AccountController")
 
 type Results struct {
 	Results []Account `json:"results"`
@@ -15,13 +22,50 @@ type Account struct {
 	Text string `json:"text"`
 }
 
-const accountListing = `{"results": [{"id": 0, "text": "Guest"},{"id": 1, "text": "Service"}]}`
-
 func GetAccountListing(c *gin.Context) {
-	arr := Results{}
-	err := json.Unmarshal([]byte(accountListing), &arr)
+
+	set := flag.NewFlagSet("getAccountListing", 0)
+	set.String("config", "", "doc")
+
+	ctx := cli.NewContext(nil, set, nil)
+	err, cfg := cmd.MakeConfig(ctx)
 	if err != nil {
-		fmt.Println(err)
+		log.Errorf("Could not make config (%v)", err)
+	}
+
+	ledger, err := ledger.New(ctx, cfg)
+	if err != nil {
+		log.Errorf("Could not make new ledger (%v)", err)
+	}
+
+	queryDB := `
+	SELECT 
+		@rownum := @rownum +1 as idx, 
+		accounts.name 
+	FROM accounts 
+		CROSS JOIN (SELECT @rownum := 0) r
+	;`
+
+	log.Debug("Querying Database")
+	rows, err := ledger.LedgerDb.Query(queryDB)
+	if err != nil {
+		log.Errorf("Could not query database (%v)", err)
+	}
+	defer rows.Close()
+
+	arr := Results{}
+	arr.Results = []Account{}
+
+	for rows.Next() {
+		//Scan one account record
+		var t Account
+		if err := rows.Scan(&t.ID, &t.Text); err != nil {
+			log.Errorf("Could not scan rows of query (%v)", err)
+		}
+		arr.Results = append(arr.Results, t)
+	}
+	if rows.Err() != nil {
+		log.Errorf("rows errored with (%v)", rows.Err())
 	}
 	c.JSON(200, &arr)
 }
