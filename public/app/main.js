55be8df79ce515d3bf1edba0a7962cc147bda089
Sean Darcy <sean@loki.network>
2020-12-10 15:17:40 +1100

    SVG needed namespace

--- public/app/main.js
+++ public/app/main.js
@@ -394,6 +394,7 @@ function updateTotal()
 }
 
 function tableCreate() {
+
     var tbdy = document.getElementById('transactionstable');
     tbdy.innerHTML = '';
     for (var i = 0; i < window.transactions.length; i++) {
@@ -404,22 +405,22 @@ function tableCreate() {
         tr.appendChild(td)
         var td = document.createElement('td');
         td.className = 'txntable';
-        var svg = document.createElement('svg');
+        var span = document.createElement('span');
+        span.appendChild(document.createTextNode(truncate(window.transactions[i].id,12)))
+        span.title=window.transactions[i].id;
+        td.appendChild(span)
+        var svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
 
         svg.setAttribute('viewBox',"0 0 16 16");
         svg.setAttribute('width',"16");
         svg.setAttribute("height","16");
-        var path = document.createElement('path');
+        var path = document.createElementNS("http://www.w3.org/2000/svg","path");
         path.setAttribute("fill-rule","evenodd");
         path.setAttribute("d","M5.75 1a.75.75 0 00-.75.75v3c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-3a.75.75 0 00-.75-.75h-4.5zm.75 3V2.5h3V4h-3zm-2.874-.467a.75.75 0 00-.752-1.298A1.75 1.75 0 002 3.75v9.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-9.5a1.75 1.75 0 00-.874-1.515.75.75 0 10-.752 1.298.25.25 0 01.126.217v9.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-9.5a.25.25 0 01.126-.217z");
 
         svg.appendChild(path)
         td.appendChild(svg)
         tr.appendChild(td)
-        var span = document.createElement('span');
-        span.appendChild(document.createTextNode(truncate(window.transactions[i].id,12)))
-        span.title=window.transactions[i].id;
-        td.appendChild(span)
         var td = document.createElement('td');
         td.className = 'txntable';
         td.appendChild(document.createTextNode(window.transactions[i]._description))
