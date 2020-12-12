da2809ca10f1cfaa8b0c1e2ec4d1d84a0bcf66bf
Sean Darcy <darcys22@gmail.com>
2020-12-12 20:15:08 +1100

    copy to clipboard button

--- public/app/main.js
+++ public/app/main.js
@@ -409,18 +409,23 @@ function tableCreate() {
         span.appendChild(document.createTextNode(truncate(window.transactions[i].id,12)))
         span.title=window.transactions[i].id;
         td.appendChild(span)
-        var svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
 
+        var svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
         svg.setAttribute('viewBox',"0 0 16 16");
         svg.setAttribute('width',"16");
         svg.setAttribute("height","16");
         var path = document.createElementNS("http://www.w3.org/2000/svg","path");
         path.setAttribute("fill-rule","evenodd");
         path.setAttribute("d","M5.75 1a.75.75 0 00-.75.75v3c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-3a.75.75 0 00-.75-.75h-4.5zm.75 3V2.5h3V4h-3zm-2.874-.467a.75.75 0 00-.752-1.298A1.75 1.75 0 002 3.75v9.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-9.5a1.75 1.75 0 00-.874-1.515.75.75 0 10-.752 1.298.25.25 0 01.126.217v9.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-9.5a.25.25 0 01.126-.217z");
+        var btn = document.createElement('button');
 
-        svg.appendChild(path)
-        td.appendChild(svg)
-        tr.appendChild(td)
+        btn.setAttribute('data-param',window.transactions[i].id);
+        btn.className = 'clipboard btn btn-sm btn-light';
+        btn.onclick = function () {copyToClipboard(this.getAttribute('data-param'));}; 
+        svg.appendChild(path);
+        btn.appendChild(svg);
+        td.appendChild(btn);
+        tr.appendChild(td);
         var td = document.createElement('td');
         td.className = 'txntable';
         td.appendChild(document.createTextNode(window.transactions[i]._description))
