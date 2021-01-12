254e62084cc37fdf515ab2df5a7b2d2d1238a67d
Sean Darcy <darcys22@gmail.com>
2021-01-12 21:06:46 +1100

    start requesting reports

--- public/app/main.js
+++ public/app/main.js
@@ -552,6 +552,44 @@ const copyToClipboard = str => {
   }
 };
 
+const reportRequest = {
+  reports: [
+  {
+    Options: {
+      Title: "TrialBalance",
+      StartDate: "2019-07-01",
+      EndDate: "2020-06-30"
+    },
+    Columns: [
+      "Date",
+      "AccountName",
+      "Description",
+      "Amount"
+    ]
+ }
+]}
+
+function getReport(reportName) {  
+  try {
+    fetch('/api/reports/',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(reportRequest)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      //Clear the page and create a table
+      //Copy the data directly into a table for it
+    })
+    .catch(error => console.error(error))
+  } catch { error => console.error(error)
+  }
+
+}
+
 function main() {
   $('#addJournal')[0].reset();
   updateTotal()
